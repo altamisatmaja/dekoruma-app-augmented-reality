fe758049963620af8b997848cbece54f6087f87f
altamisatmaja <altamisfattah16@gmail.com>
2024-02-15 01:16:45 +0700

    feat: setup

---
+++ lib/home_screen.blade.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatefulWidget {
+  const HomeScreenPage({super.key});
+
+  @override
+  State<HomeScreenPage> createState() => _HomeScreenPageState();
+}
+
+class _HomeScreenPageState extends State<HomeScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
